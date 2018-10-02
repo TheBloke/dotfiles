283ad7dcac24311c22181a6434bbcde3bb68f5c3
Tom Jobbins <784313+TheBloke@users.noreply.github.com>
2018-10-02 02:09:43 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
-#!/bin/sh
+#!/usr/bin/bash
 
 if [ ! -d "$HOME/.yadr" ]; then
     echo "Installing YADR for the first time"
-    git clone --depth=1 https://github.com/skwp/dotfiles.git "$HOME/.yadr"
+    git clone --depth=1 https://github.com/TheBloke/dotfiles.git "$HOME/.yadr"
     cd "$HOME/.yadr"
     [ "$1" = "ask" ] && export ASK="true"
     rake install
